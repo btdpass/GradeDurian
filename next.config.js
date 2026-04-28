91b72914b61e93ea7d31d5c4e3e94a399c6d7a67
btdpass <catqverycool@gmail.com>
2026-04-27 22:54:35 -0400

    config update

--- next.config.js
+++ next.config.js
@@ -18,7 +18,7 @@ const nextConfig = withPwa({
 	env: {
 		NEXT_PUBLIC_BASE_PATH: basePath,
 		NEXT_PUBLIC_GITHUB_REPO: process.env.GITHUB_REPOSITORY || '',
-		NEXT_PUBLIC_API_URL: "https://cloudproxy.gradedurian.workers.dev",
+		NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "https://cloudproxy.gradedurian.workers.dev",
 	},
 });
 
